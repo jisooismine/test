01f79a48958b65951648e6bbb03b9cf9c5dab704
jisooismine <15927338773@163.com>
2022-03-08 10:20:38 +0800

    本次提交实现了数据的饼状图显示 对数据可视化有了进一步的理解，完善了柱状图显示上的一些不足 包括y轴尺度 x轴显示文字不全等

--- news_system/src/view/newssandbox/home/Home.js
+++ news_system/src/view/newssandbox/home/Home.js
@@ -11,8 +11,12 @@ const { Meta } = Card;
 export default function Home() {
     const [viewList, setviewList] = useState([])
     const [starList, setstarList] = useState([])
-
+    const [allList, setallList] = useState([])
+    const [visible, setvisible] = useState(false)
+    const [pieChart, setpieChart] = useState(null)
     const barRef = useRef()
+    const pieRef = useRef()
+
     useEffect(() => {
         axios.get("http://localhost:5000/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res => {
             // console.log(res.data)
@@ -33,10 +37,14 @@ export default function Home() {
             // console.log(res.data)
             // console.log()
             renderBarView(_.groupBy(res.data,item=>item.category.title))
+            setallList(res.data)
         })
+        return ()=>{
+            window.onresize = null
+        }
     }, [])
 
-    const renderBarView = (obj)=>{
+    const renderBarView = (obj) => {
         var myChart = Echarts.init(barRef.current);
 
         // 指定图表的配置项和数据
@@ -49,18 +57,85 @@ export default function Home() {
                 data: ['数量']
             },
             xAxis: {
-                data: Object.keys(obj)
+                data: Object.keys(obj),
+                axisLabel: {
+                    rotate: "45",
+                    interval: 0
+                }
             },
-            yAxis: {},
+            yAxis: {
+                minInterval: 1
+            },
             series: [{
                 name: '数量',
                 type: 'bar',
-                data: Object.values(obj).map(item=>item.length)
+                data: Object.values(obj).map(item => item.length)
             }]
         };
 
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
+
+
+        window.onresize = () => {
+            // console.log("resize")
+            myChart.resize()
+        }
+    }
+
+    const renderPieView = (obj) => {
+        //数据处理工作
+
+        var currentList =allList.filter(item=>item.author===username)
+        var groupObj = _.groupBy(currentList,item=>item.category.title)
+        var list = []
+        for(var i in groupObj){
+            list.push({
+                name:i,
+                value:groupObj[i].length
+            })
+        }
+        var myChart;
+        if(!pieChart){
+            myChart = Echarts.init(pieRef.current);
+            setpieChart(myChart)
+        }else{
+            myChart = pieChart
+        }
+        var option;
+
+        option = {
+            title: {
+                text: '当前用户新闻分类图示',
+                // subtext: '纯属虚构',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+            },
+            series: [
+                {
+                    name: '发布数量',
+                    type: 'pie',
+                    radius: '50%',
+                    data: list,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+
+        option && myChart.setOption(option);
+
     }
 
     const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem("token"))
@@ -100,7 +175,14 @@ export default function Home() {
                             />
                         }
                         actions={[
-                            <SettingOutlined key="setting" />,
+                            <SettingOutlined key="setting" onClick={() => {
+                                setTimeout(() => {
+                                    setvisible(true)
+
+                                    // init初始化
+                                    renderPieView()
+                                }, 0)
+                            }} />,
                             <EditOutlined key="edit" />,
                             <EllipsisOutlined key="ellipsis" />,
                         ]}
@@ -120,10 +202,26 @@ export default function Home() {
                     </Card>
                 </Col>
             </Row>
+            <Drawer
+                width="500px"
+                title="个人新闻分类"
+                placement="right"
+                closable={true}
+                onClose={() => {
+                    setvisible(false)
+                }}
+                visible={visible}
+            >
+                <div ref={pieRef} style={{
+                    width: '100%',
+                    height: "400px",
+                    marginTop: "30px"
+                }}></div>
+            </Drawer>
 
 
             <div ref={barRef} style={{
-                width:'100%',
+                width: '100%',
                 height: "400px",
                 marginTop: "30px"
             }}></div>

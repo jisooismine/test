bbfbf21aeb8b2f10fc18285776465f3acd76d216
jisooismine <15927338773@163.com>
2022-03-09 10:10:51 +0800

    本次提交初步完成游客访问，不需要登录也能进入阅读新闻的界面

--- news_system/src/router/indexRouter.js
+++ news_system/src/router/indexRouter.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {HashRouter,Navigate,Routes,Route} from 'react-router-dom'
 import Login from '../view/login/Login'
+import Detail from '../view/news/Detail'
+import News from '../view/news/News'
 import NewsSandBox from '../view/newssandbox/NewsSandBox'
 export default function IndexRouter() {
     return (
         <HashRouter>
            <Routes>
            <Route path="/login" element={<Login/>}/>
+           <Route path="/news" element={<News/>}></Route>
+           <Route path="/detail/:id" element={<Detail/>}/>
                 {/* <Route path="/" component={NewsSandBox}/> */}
                 <Route path="/*" element={localStorage.getItem("token")? <NewsSandBox/>:<Navigate to="/login"/>}/>
            </Routes>
